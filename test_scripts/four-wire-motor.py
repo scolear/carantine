305c0f94236247bbb89230585e8d7441fe981114
scolear <bobtera@gmail.com>
2020-04-09 20:57:39 +0200

    update fourwiremotostest

--- test_scripts/four-wire-motor.py
+++ test_scripts/four-wire-motor.py
@@ -3,11 +3,11 @@ from time import sleep
  
 #///////////////// Define Motor Driver GPIO Pins /////////////////
 # Motor A, Left Side GPIO CONSTANTS
-PWM_FORWARD_LEFT_PIN = 12	# IN1 - Forward Drive
-PWM_REVERSE_LEFT_PIN = 18	# IN2 - Reverse Drive
+PWM_FORWARD_LEFT_PIN = 18	# IN1 - Forward Drive
+PWM_REVERSE_LEFT_PIN = 12	# IN2 - Reverse Drive
 # Motor B, Right Side GPIO CONSTANTS
-PWM_FORWARD_RIGHT_PIN = 13	# IN1 - Forward Drive
-PWM_REVERSE_RIGHT_PIN = 19	# IN2 - Reverse Drive
+PWM_FORWARD_RIGHT_PIN = 19	# IN1 - Forward Drive
+PWM_REVERSE_RIGHT_PIN = 13	# IN2 - Reverse Drive
  
 # Initialise objects for H-Bridge PWM pins
 # Set initial duty cycle to 0 and frequency to 1000
@@ -25,16 +25,16 @@ def allStop():
 	reverseRight.value = 0
  
 def forwardDrive():
-	forwardLeft.value = 1.0
+	forwardLeft.value = 0.8
 	reverseLeft.value = 0
-	forwardRight.value = 1.0
+	forwardRight.value = 0.8
 	reverseRight.value = 0
  
 def reverseDrive():
 	forwardLeft.value = 0
-	reverseLeft.value = 1.0
+	reverseLeft.value = 0.8
 	forwardRight.value = 0
-	reverseRight.value = 1.0
+	reverseRight.value = 0.8
  
 def spinLeft():
 	forwardLeft.value = 0
@@ -43,10 +43,10 @@ def spinLeft():
 	reverseRight.value = 0
  
 def SpinRight():
-	forwardLeft.value = 1.0
+	forwardLeft.value = 0.8
 	reverseLeft.value = 0
 	forwardRight.value = 0
-	reverseRight.value = 1.0
+	reverseRight.value = 0.8
  
 def forwardTurnLeft():
 	forwardLeft.value = 0.2
