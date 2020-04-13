769038bc3253d246b0972a70322234ba9e014d9a
scolear <bobtera@gmail.com>
2020-04-13 14:46:59 +0200

    create openCV skeleton. Find best method for eventloop handling, keypress events; etc;

--- carantine_project/main.cpp
+++ carantine_project/main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 #include <wiringPi.h>
+#include <opencv2/highgui.hpp>
 
 #include "movement/trisonarhandler.h"
 
 int main()
 {
+
     wiringPiSetupGpio();
 
     TriSonarHandler sensors;
 
-    while (true) {
+    cv::namedWindow("Carantine");
+
+    bool run = true;
+
+    while (run) {
 
         for (auto distance : sensors.readDistances())
         {
             std::cout << distance << " ";
         }
         std::cout << std::endl;
-        delay(1000);
+        delay(100);
+
+        cv::imshow("Carantine", cv::Mat(100, 100, CV_8UC3, cv::Scalar(0, 0, 0)));
+
+        int c = cv::waitKey(25);
+        if (c == 'q')
+            break;
+
     }
     return 0;
 }
