a7a168956fce3552a9a32aa8ca55dab7a1a5162d
scolear <bobtera@gmail.com>
2020-04-14 16:33:47 +0200

    strange beginnings

--- carantine_project/main.cpp
+++ carantine_project/main.cpp
@@ -2,7 +2,9 @@
 #include <wiringPi.h>
 #include <opencv2/highgui.hpp>
 
+#include "main.h"
 #include "movement/trisonarhandler.h"
+
 
 int main()
 {
@@ -12,24 +14,50 @@ int main()
     TriSonarHandler sensors;
 
     cv::namedWindow("Carantine");
+    cv::imshow("Carantine", cv::Mat(100, 100, CV_8UC3, cv::Scalar(0, 0, 0)));
 
     bool run = true;
+    Mode mode = Mode::MANUAL;
+    bool displayDistances = true;
 
-    while (run) {
-
-        for (auto distance : sensors.readDistances())
-        {
-            std::cout << distance << " ";
+    while (run)
+    {
+        switch (mode) {
+            case Mode::MANUAL:
+            {
+                std::cout << "Manual mode" << std::endl;
+                break;
+            }
+            case Mode::AUTO:
+            {
+                std::cout << "Auto mode" << std::endl;
+                break;
+            }
         }
-        std::cout << std::endl;
+
+        if (displayDistances)
+        {
+            for (auto distance : sensors.readDistances())
+            {
+                std::cout << distance << " ";
+            }
+            std::cout << std::endl;
+        }
+
         delay(100);
 
-        cv::imshow("Carantine", cv::Mat(100, 100, CV_8UC3, cv::Scalar(0, 0, 0)));
-
         int c = cv::waitKey(25);
-        if (c == 'q')
-            break;
-
+        
+        if (c == 'q') {
+            run = false;
+        } else if (c == 'm') {
+            mode = Mode::MANUAL;
+        } else if (c == 'a') {
+            mode = Mode::AUTO;
+        } else if (c == 'd') {
+            displayDistances = !displayDistances;
+        }
     }
+
     return 0;
 }
